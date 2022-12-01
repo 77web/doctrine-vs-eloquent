dee0077b811175c867aebfcb1aad7960a2da117b
77web <info@77-web.com>
2022-12-02 00:06:03 +0900

    [Doctrine] added symfony/cache for ORMSetup requires cache

--- Doctrine/Usecase/bootstrap.php
+++ Doctrine/Usecase/bootstrap.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\ORMSetup;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
 
 $config = ORMSetup::createAttributeMetadataConfiguration(
     paths: array(__DIR__."/../Entity"),
     isDevMode: true,
+    cache: new ArrayAdapter(),
 );
 $conn = array(
     'driver' => 'pdo_sqlite',
