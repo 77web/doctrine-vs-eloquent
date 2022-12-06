27b56217ea6eb03783f62c410be48e350589d6d7
77web <info@77-web.com>
2022-12-07 00:49:01 +0900

    [Doctrine] defined relationship from owning side

--- Doctrine/Entity/Author.php
+++ Doctrine/Entity/Author.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity]
@@ -17,6 +19,14 @@ class Author
 
     #[ORM\Column(type: 'string', length: 255)]
     private string $name;
+
+    #[ORM\OneToMany(targetEntity: Book::class)]
+    private Collection $books;
+
+    public function __construct()
+    {
+        $this->books = new ArrayCollection();
+    }
 
     public function getId(): string
     {
@@ -36,5 +46,18 @@ class Author
     public function setName(string $name): void
     {
         $this->name = $name;
+    }
+
+    public function getBooks(): Collection
+    {
+        return $this->books;
+    }
+
+    public function addBook(Book $book): void
+    {
+        if (!$this->books->contains($book)) {
+            $book->setAuthor($this);
+            $this->books->add($book);
+        }
     }
 }
