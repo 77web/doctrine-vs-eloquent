ba480bcc976aa9ef5cdcccb4042ac1ff6bb4fc50
77web <info@77-web.com>
2022-12-07 00:55:15 +0900

    [Eloquent] defined relationship from owning side

--- Eloquent/Models/Author.php
+++ Eloquent/Models/Author.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -11,10 +12,16 @@ use Illuminate\Database\Eloquent\Model;
  *
  * @property int $id
  * @property string $name
+ * @property Collection<Book> $books
  */
 class Author extends Model
 {
     public $fillable = [
         'name',
     ];
+
+    public function books()
+    {
+        return $this->hasMany(Book::class);
+    }
 }
