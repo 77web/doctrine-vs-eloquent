280a4d80c66fce24bc08993cf7015902917a09d2
77web <info@77-web.com>
2022-12-18 00:35:34 +0900

    [sql] added missing description field for books table

--- sqlite_books.php
+++ sqlite_books.php
@@ -16,6 +16,6 @@ $conn = $em->getConnection();
 for ($i = 1; $i <= 5; $i++) {
     $conn->insert('authors', ['id' => $i, 'name' => 'author'.$i]);
     for ($j = 1; $j <= 10; $j++) {
-        $conn->insert('books', ['id' => $j, 'author_id' => $i, 'title' => 'book'.$j, 'price' => 100 * $i * $j]);
+        $conn->insert('books', ['id' => $j, 'author_id' => $i, 'title' => 'book'.$j, 'description' => 'description'.$i, 'price' => 100 * $i * $j]);
     }
 }
